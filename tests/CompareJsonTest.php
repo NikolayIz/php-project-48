1822fbe4adb4cd04c079d94699c0b8cbd1ed9348
NikolayIz <nikolay97altov@gmail.com>
2025-07-12 18:57:48 +0300

    add test genDiff

--- tests/CompareJsonTest.php
+++ tests/CompareJsonTest.php
@@ -1,13 +1,29 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 use PHPUnit\Framework\TestCase;
-use Hexlet\Code\Gendiff\genDiff;
+use function Hexlet\Code\Gendiff\genDiff;
 
 class CompareJsonTest extends TestCase
 {
-    public function testSomething()
+    public function testGenDiff()
     {
-        $this->assertTrue(true); // тест-заглушка
+        $pathToFile1 = __DIR__ . '/fixtures/file1.json';
+        $pathToFile2 = __DIR__ . '/fixtures/file2.json';
+        $expectedPath = __DIR__ . '/fixtures/expected.txt';
+        $expected = file_get_contents($expectedPath);
+        $actualResult = genDiff($pathToFile1, $pathToFile2);
+        $this->assertEquals(trim($expected), trim($actualResult));
+    }
+
+    public function testGenDiffSame()
+    {
+        $pathToFile1 = __DIR__ . '/fixtures/file1same.json';
+        $pathToFile2 = __DIR__ . '/fixtures/file2same.json';
+        $expectedPath = __DIR__ . '/fixtures/expectedSameFiles.txt';
+        $expected = file_get_contents($expectedPath);
+        $actualResult = genDiff($pathToFile1, $pathToFile2);
+        $this->assertEquals(trim($expected), trim($actualResult));
     }
 }
-

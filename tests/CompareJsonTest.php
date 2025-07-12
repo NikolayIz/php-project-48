505462c9534e8106972343f900f60ab3ff1fcadd
NikolayIz <nikolay97altov@gmail.com>
2025-07-12 17:51:28 +0300

    add dir tests

---
+++ tests/CompareJsonTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Hexlet\Code\Gendiff\genDiff;
+
+class CompareJsonTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true); // тест-заглушка
+    }
+}
+

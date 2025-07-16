33aaca366408bc8237b7780f50593ece37801e35
NikolayIz <nikolay97altov@gmail.com>
2025-07-17 00:01:25 +0300

    add testFormatValue

---
+++ tests/FormatValueTest.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use function Hexlet\Code\Gendiff\formatValue;
+
+class FormatValueTest extends TestCase
+{
+    #[DataProvider('valuesProvider')]
+    public function testFormatValue($expected, $argument)
+    {
+        $value = 'test';
+        $this->assertEquals($expected, formatValue($argument));
+    }
+
+    public static function valuesProvider(): array
+    {
+        return [
+            ['true', true],
+            ['false', false],
+            ['null', null],
+            ['randomValue', 'randomValue'],
+        ];
+    }
+}
